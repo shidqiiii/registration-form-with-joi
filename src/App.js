4591c021f7530e5246ef25acd448641ac0dbcd36
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-06-26 20:43:06 +0700

    update handleSubmit & joi validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import Joi from 'joi';
 import React, { useState } from 'react';
 import { Card, Container, Form, Button, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   const [data, setData] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    phone: ""
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phoneNumber: ""
   });
 
   // HandleChange
@@ -18,14 +19,29 @@ function App() {
     })
   }
 
-
   // Schema & Validation
+  const schema = Joi.object().keys({
+    firstName: Joi.string().min(5).regex(/^[a-zA-Z]+$/).required(),
+    lastName: Joi.string().min(5).regex(/^[a-zA-Z]+$/).required(),
+    emailAddress: Joi.string().email({ tlds: { allow: false } }).regex(/^[a-zA-Z]/).required(),
+    phoneNumber: Joi.string().min(8).max(15).regex(/^08[1-9][0-9]{4}/).required()
+  })
+
+
   // HandleSubmit
+  const handleSubmit = () => {
+    console.log(schema.validate(data, { abortEarly: false }));
+  }
+
   return (
     <Container className='mt-5'>
       <Card style={{ maxWidth: "70%" }} className='mx-auto'>
         <Card.Body>
-          <Form className='p-3'>
+          <Form className='p-3'
+            onSubmit={(event) => {
+              event.preventDefault();
+              handleSubmit();
+            }}>
             <Card.Title className='text-center mb-3'>Form Alumni</Card.Title>
             <Row>
               <Col md={12} lg={6}>
@@ -34,7 +50,7 @@ function App() {
                   <Form.Control
                     type="text"
                     placeholder="Enter first name"
-                    onChange={(event) => { handleChange("firstname", event.target.value) }} />
+                    onChange={(event) => { handleChange("firstName", event.target.value) }} />
                 </Form.Group>
               </Col>
               <Col md={12} lg={6}>
@@ -43,7 +59,7 @@ function App() {
                   <Form.Control
                     type="text"
                     placeholder="Enter last name"
-                    onChange={(event) => { handleChange("lastname", event.target.value) }} />
+                    onChange={(event) => { handleChange("lastName", event.target.value) }} />
                 </Form.Group>
               </Col>
             </Row>
@@ -52,14 +68,14 @@ function App() {
               <Form.Control
                 type="email"
                 placeholder="Enter email address"
-                onChange={(event) => { handleChange("email", event.target.value) }} />
+                onChange={(event) => { handleChange("emailAddress", event.target.value) }} />
             </Form.Group>
             <Form.Group className="mb-4">
               <Form.Label>Phone Number</Form.Label>
               <Form.Control
                 type="number"
                 placeholder="Enter phone number"
-                onChange={(event) => { handleChange("phone", event.target.value) }} />
+                onChange={(event) => { handleChange("phoneNumber", event.target.value) }} />
             </Form.Group>
             <Button variant="primary" type="submit" style={{ width: "100%" }}>Submit</Button>
           </Form>
